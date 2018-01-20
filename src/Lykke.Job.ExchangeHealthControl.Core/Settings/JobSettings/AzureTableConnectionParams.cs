6b5b08231895bf8db63e840891d4aa8a98ae179c
Alexey Andreev <sailen.alex@gmail.com>
2018-01-20 14:01:00 +0300

    Fix reference issues

--- src/Lykke.Job.ExchangeHealthControl.Core/Settings/JobSettings/AzureTableConnectionParams.cs
+++ src/Lykke.Job.ExchangeHealthControl.Core/Settings/JobSettings/AzureTableConnectionParams.cs
@@ -4,9 +4,6 @@ namespace Lykke.Job.ExchangeHealthControl.Core.Settings.JobSettings
 {
     public class AzureTableConnectionParams
     {
-        [Optional]
-        public string ConnectionString { get; set; }
-
         public string TableName { get; set; }
     }
 }
